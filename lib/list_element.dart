1e101a3a7096206fbcd641e066eed3a7db5f9ce4
Adrian <kaleciarz@gmail.com>
2019-01-29 23:29:45 +0100

    Added the dismissing of the client

--- lib/list_element.dart
+++ lib/list_element.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/client_model.dart';
 import 'package:my_first_app/details_screen.dart';
+import 'package:my_first_app/database.dart';
 
 class ListElement extends StatelessWidget {
   final Client client;
@@ -9,19 +10,26 @@ class ListElement extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        leading: CircleAvatar(
-          child: Text(client.name.substring(0,1)),
-        ),
-        title:  Text(client.company),
-        subtitle: Text(client.email),
-        onTap: () { 
-            Navigator.push(context,
-            MaterialPageRoute(
-              builder: (context) => DetailsScreen(client)
-            ),
-          );
-        }
+    return Dismissible(
+          key: UniqueKey(),
+          background: Container(color: Colors.red),
+          onDismissed: (direction) {
+            DBProvider.db.deleteClient(client.id);
+          },
+          child: ListTile(
+          leading: CircleAvatar(
+            child: Text(client.name.substring(0,1)),
+          ),
+          title:  Text(client.company),
+          subtitle: Text(client.email),
+          onTap: () { 
+              Navigator.push(context,
+              MaterialPageRoute(
+                builder: (context) => DetailsScreen(client)
+              ),
+            );
+          }
+      ),
     );
   }
 }
